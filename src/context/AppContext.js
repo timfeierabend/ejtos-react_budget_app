fd259ff0de08aaf689c69e2bac11e758e6a6ce50
Timothy Feierabend <tim.feierabend@gmail.com>
2023-12-15 13:17:20 -0600

    Re-factored to get rid of all instances for using 'useReducer' properly
    useReducer() should not modify action or state. StrictMode checks for this.
    
    created deep copies of arrays so I can modify without touching current state.

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,59 +2,54 @@ import React, { createContext, useReducer } from 'react';
 
 // 5. The reducer - this is used to update the state, based on the action
 export const AppReducer = (state, action) => {
-    let budget = 0;
     switch (action.type) {
         case 'ADD_EXPENSE':
             let total_budget = 0;
-            total_budget = state.expenses.reduce(
-                (previousExp, currentExp) => {
-                    return previousExp + currentExp.cost
-                },0
-            );
-            total_budget = total_budget + action.payload.cost;
-            action.type = "DONE";
+            total_budget = state.expenses.reduce((total, item) => {
+                return total + item.cost;
+            }, 0);
+            total_budget += action.payload.cost;
             if(total_budget <= state.budget) {
-                total_budget = 0;
-                state.expenses.map((currentExp)=> {
+                const add_expenses = JSON.parse(JSON.stringify(state.expenses));
+                add_expenses.map((currentExp)=> {
                     if(currentExp.name === action.payload.name) {
-                        currentExp.cost = action.payload.cost + currentExp.cost;
+                        currentExp.cost += action.payload.cost;
                     }
                     return currentExp
                 });
                 return {
                     ...state,
+                    expenses: add_expenses
                 };
             } else {
                 alert("Cannot increase the allocation! Out of funds");
                 return {
                     ...state
-                }
+                };
             }
         case 'RED_EXPENSE':
-            const red_expenses = state.expenses.map((currentExp)=> {
-                if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
-                    currentExp.cost =  currentExp.cost - action.payload.cost;
-                    budget = state.budget + action.payload.cost
+            const red_expenses = JSON.parse(JSON.stringify(state.expenses));
+            red_expenses.map((currentExp) => {
+                if (currentExp.name === action.payload.name) {
+                    currentExp.cost -= action.payload.cost;
                 }
-                return currentExp
-            })
-            action.type = "DONE";
+                return currentExp;
+            });
             return {
                 ...state,
-                expenses: [...red_expenses],
+                expenses: red_expenses
             };
         case 'DELETE_EXPENSE':
-            state.expenses.map((currentExp)=> {
+            const del_expenses = JSON.parse(JSON.stringify(state.expenses));
+            del_expenses.map((currentExp) => {
                 if (currentExp.name === action.payload) {
-                    budget = state.budget + currentExp.cost
-                    currentExp.cost =  0;
+                    currentExp.cost = 0;
                 }
-                return currentExp
-            })
-            action.type = "DONE";
+                return currentExp;
+            });
             return {
                 ...state,
-                budget
+                expenses: del_expenses
             };
         case 'SET_BUDGET':
             return {
@@ -68,7 +63,7 @@ export const AppReducer = (state, action) => {
             };
 
         default:
-            return state;
+            return { ...state };
     }
 };
 
@@ -94,10 +89,9 @@ export const AppProvider = (props) => {
     // 4. Sets up the app state. takes a reducer, and an initial state
     const [state, dispatch] = useReducer(AppReducer, initialState);
     let remaining = 0;
-    let totalExpenses = 0;
 
     if (state.expenses) {
-        totalExpenses = state.expenses.reduce((total, item) => {
+        const totalExpenses = state.expenses.reduce((total, item) => {
             return total + item.cost;
         }, 0);
         remaining = state.budget - totalExpenses;
@@ -109,7 +103,6 @@ export const AppProvider = (props) => {
                 expenses: state.expenses,
                 budget: state.budget,
                 remaining: remaining,
-                totalExpenses: totalExpenses,
                 dispatch,
                 currency: state.currency
             }}
